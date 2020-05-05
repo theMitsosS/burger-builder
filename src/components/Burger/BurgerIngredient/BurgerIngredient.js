11e3a0147ab29e8e2b22ffd0a60e65ef93ddcb2d
Dimitris Selalmazidis <dimitris.selalmazidis@aexp.com>
2020-05-05 08:25:56 +0100

    converted BurgerIngredient.js, modal.js & OrderSummary.js to functional components

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,40 +1,38 @@
-import React, {Component} from 'react';
+import React from 'react';
 import classes from './BurgerIngredients.module.css';
 import PropTypes from 'prop-types';
 
-class BurgerIngredient extends Component {
-  render() {
-    let ingredient = null;
-    switch (this.props.type) {
-      case 'bread-bottom':
-        ingredient = <div className={classes.BreadBottom}> </div>;
-        break;
-      case 'bread-top':
-        ingredient = (
-          <div className={classes.BreadTop}>
-            <div className={classes.Seeds1}></div>
-            <div className={classes.Seeds2}></div>
-          </div>
-        );
-        break;
-      case 'meat':
-        ingredient = <div className={classes.Meat}></div>;
-        break;
-      case 'bacon':
-        ingredient = <div className={classes.Bacon}></div>;
-        break;
-      case 'cheese':
-        ingredient = <div className={classes.Cheese}></div>;
-        break;
-      case 'salad':
-        ingredient = <div className={classes.Salad}></div>;
-        break;
-      default:
-        ingredient = null;
-    }
-    return ingredient;
+const BurgerIngredient =(props)=> {
+  let ingredient;
+  switch (props.type) {
+    case 'bread-bottom':
+      ingredient = <div className={classes.BreadBottom}> </div>;
+      break;
+    case 'bread-top':
+      ingredient = (
+        <div className={classes.BreadTop}>
+          <div className={classes.Seeds1}/>
+          <div className={classes.Seeds2}/>
+        </div>
+      );
+      break;
+    case 'meat':
+      ingredient = <div className={classes.Meat}/>;
+      break;
+    case 'bacon':
+      ingredient = <div className={classes.Bacon}/>;
+      break;
+    case 'cheese':
+      ingredient = <div className={classes.Cheese}/>;
+      break;
+    case 'salad':
+      ingredient = <div className={classes.Salad}/>;
+      break;
+    default:
+      ingredient = null;
   }
-}
+  return ingredient;
+};
 
 BurgerIngredient.propTypes={
   type: PropTypes.string.isRequired,
--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,38 +1,36 @@
-import React, {Component} from 'react';
+import React from 'react';
 import Aux from '../../../hoc/Aux/Aux';
 import Button from '../../UI/button/Button';
-class OrderSummary extends Component {
- 	render() {
-    const ingredientSummary = Object.entries(this.props.ingredients).map(
-        (ingredient) => {
-          return (
-            <li key={ingredient[0]}>
-              <span style={{textTransform: 'capitalize'}}>{ingredient[0]}</span>
+const OrderSummary =(props)=> {
+  const ingredientSummary = Object.entries(props.ingredients).map(
+      (ingredient) => {
+        return (
+          <li key={ingredient[0]}>
+            <span style={{textTransform: 'capitalize'}}>{ingredient[0]}</span>
 						:{ingredient[1]}
-            </li>
-          );
-        },
-    );
-    return (
-      <Aux>
-        <h3>Your Order</h3>
-        <p>A delicious burger with the following ingredients:</p>
-        <ul>{ingredientSummary}</ul>
-        <p>
-          <strong>Total price: {this.props.price.toFixed(2)} </strong>
-        </p>
-        <p>Continue to checkout?</p>
-        <Button
-          btnType="Danger"
-          onClick={() => this.props.cancelPurchase(false)}>
+          </li>
+        );
+      },
+  );
+  return (
+    <Aux>
+      <h3>Your Order</h3>
+      <p>A delicious burger with the following ingredients:</p>
+      <ul>{ingredientSummary}</ul>
+      <p>
+        <strong>Total price: {props.price.toFixed(2)} </strong>
+      </p>
+      <p>Continue to checkout?</p>
+      <Button
+        btnType="Danger"
+        onClick={() => props.cancelPurchase(false)}>
 					CANCEL
-        </Button>
-        <Button btnType="Success" onClick={this.props.continuePurchase}>
+      </Button>
+      <Button btnType="Success" onClick={props.continuePurchase}>
 					CONTINUE
-        </Button>
-      </Aux>
-    );
-  }
-}
+      </Button>
+    </Aux>
+  );
+};
 
 export default OrderSummary;
